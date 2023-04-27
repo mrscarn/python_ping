6251ff4400ab1b7a178fa38cea94b777e8a75096
clt <christoffer.lext@gmail.com>
2023-04-27 08:57:56 +0200

    First commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import platform
 
 print ("PING SCANNER")
 
-#Use different param depending if it's linux or windows
+#Sätt parameter n om Linux och c om Windows
 param = '-n' if platform.system().lower()=='windows' else '-c'
 
 #Ping function
@@ -11,19 +11,18 @@ def ping(host):
     command = ["ping", param, "1", host]
     return subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
 
-#Create a single var for pinging single host
+#Pinga enskild host
 host = "google.com"
 
-#If statement of wht the ping function returns
 if ping(host):
     print("Ping successfull!")
 else: 
     print("Ping unsuccessfull")
     
-#Create a list of hosts for pinging    
+#En lista för hostar som ska pingas  
 host_list = ["google.com", "gt.se", "amazon.com"]
 
-#For loop for printing and pinging a hosts in the previous host list
+#Loop som pingar varje host i listan
 for host in host_list:
     print(host)
     ping(host)
